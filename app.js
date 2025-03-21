33748580d85fde6611449131ba6a8de23f6d9912
vedant singh <93176679+Lonewolf123457499@users.noreply.github.com>
2025-03-21 14:27:00 +0530

    [vedant singh] Added:UC3

--- app.js
+++ app.js
@@ -30,3 +30,23 @@ switch (empCheck) {
 }
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage)
+
+function getworkingHours(empCheck) {
+
+    switch (empCheck) {
+
+        case IS_PART_TIME:
+
+            return PART_TIME_HOURS;
+
+        case IS_FULL_TIME:
+
+            return FULL_TIME_HOURS;
+
+        default:
+
+            return 0;
+
+    }
+
+}
